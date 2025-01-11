02aa94cda5c678788f98116af557a9646726f6cb
pratinha <victorsousaprata@gmail.com>
2025-01-11 13:00:41 -0300

    refactor(imports): use alias for data_transformation module

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,10 +1,9 @@
 from pandas import DataFrame
 
-
-from data_extraction import get_pokemons_dataframe
+import data_extraction as e
 import data_transformation as t
 
-df_pokemons = get_pokemons_dataframe(10)
+df_pokemons = e.get_pokemons_dataframe(10)
 df_pokemons['Categoria'] = df_pokemons['Experiência Base'].apply(t.categorize_base_experience)
 df_types = t.get_df_pokemon_by_type(df_pokemons)
 t.plot_pokemon_type_distribution(df_types)
