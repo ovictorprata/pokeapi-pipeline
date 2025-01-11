2b2abb54f1287525711aaa2c2300a3a2b6e7bf25
pratinha <victorsousaprata@gmail.com>
2025-01-11 16:45:57 -0300

    feat(logging): add start process logging in analyze_data

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -1,4 +1,7 @@
+import logging
+
 def get_top_5_highest_base_experience(df_pokemons):
+    logging.info('Calculating the top 5 Pokémon with the highest base experience...')
     top_5 = df_pokemons.sort_values(by='Experiência Base', ascending=False).head(5)
     top_5_names = top_5[['Nome']].reset_index(drop=True)
     top_5_names.index += 1 
@@ -8,6 +11,7 @@ def get_top_5_highest_base_experience(df_pokemons):
     return top_5_names
 
 def get_attack_defense_hp_mean_per_type(df_pokemons):
+    logging.info('Calculating the average attack, defense, and HP per type...')
     df_pokemons = df_pokemons.explode('Tipos')
     mean_values = df_pokemons.groupby('Tipos')[['HP', 'Ataque', 'Defesa']].mean()
     mean_values = mean_values.reset_index()
--- src/pipeline.py
+++ src/pipeline.py
@@ -15,21 +15,24 @@ def run_pipeline():
         # tarefa 1
         df_pokemons = ext.get_pokemons_dataframe(10)
         # tarefa 2
+        logging.info('Categorizing pokemons.')
         df_pokemons['Categoria'] = df_pokemons['Experiência Base'].apply(t.categorize_base_experience)
         df_types = t.get_df_pokemon_by_type(df_pokemons)
         t.generate_chart_pokemon_type_distribution(df_types)
         # tarefa 3
+        df_means = a.get_attack_defense_hp_mean_per_type(df_pokemons)
+        df_means.to_csv('reports/means_attack_defense_hp.csv')
         print('\n Médias de HP, Ataque e Defesa')
         print('-' * 70)
-        print(a.get_attack_defense_hp_mean_per_type(df_pokemons))
+        print(df_means)
         print('-' * 70)
-        print('Top 5 pokemons com maiores experiências base')
+        df_top_5 = a.get_top_5_highest_base_experience(df_pokemons)
+        df_top_5.to_csv('reports/top_5_base_experience.csv')
+        print('\nTop 5 pokemons com maiores experiências base')
         print('-' * 70)
-        print(a.get_top_5_highest_base_experience(df_pokemons))
+        print(df_top_5)
         print('-' * 70)
 
-        a.get_attack_defense_hp_mean_per_type(df_pokemons).to_csv('reports/means_attack_defense_hp.csv')
-        a.get_top_5_highest_base_experience(df_pokemons).to_csv('reports/top_5_base_experience.csv')
         logging.info('Pipeline completed successfully.')
     except Exception as e:
         logging.error(f"Pipeline failed: {e}")
