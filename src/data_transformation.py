0c1937b82af62ad9037bc249770cd9c2676825af
pratinha <victorsousaprata@gmail.com>
2025-01-11 11:44:25 -0300

    feat(data-transformation): create DataFrame with Pokémon count by type

--- src/data_transformation.py
+++ src/data_transformation.py
@@ -1,3 +1,5 @@
+from pandas import DataFrame
+
 def categorize_base_experience(base_experinece):
     category =  None
     if base_experinece < 50:
@@ -7,3 +9,11 @@ def categorize_base_experience(base_experinece):
     else:
         category = 'Forte'
     return category
+
+
+def get_df_pokemon_by_type(df_pokemons):
+    exploded_types = df_pokemons['Tipos'].explode()
+    df_type_count = exploded_types.value_counts().reset_index()
+    df_type_count.columns = ['Tipos', 'Quantidade']
+    return df_type_count
+ 
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,9 @@
+from pandas import DataFrame
+
 from data_extraction import get_pokemons_dataframe
-from data_transformation import categorize_base_experience
+from data_transformation import categorize_base_experience, get_df_pokemon_by_type
 
 df_pokemons = get_pokemons_dataframe(10)
 df_pokemons['Categoria'] = df_pokemons['Experiência Base'].apply(categorize_base_experience)
+print(get_df_pokemon_by_type(df_pokemons))
+
